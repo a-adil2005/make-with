eb875e4aa997599de66563add5a7fbf69aeb9fbf
Mohd Adil Ansari <220619237+a-adil2005@users.noreply.github.com>
2026-07-16 15:28:23 +0530

    login fix

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Login.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
   return (
     <div className="login-page">
       {/* Back button positioned absolutely */}
-      <Link to="/" className="close-btn">×</Link>
+      <Link to="/" className="close-btn">
+        ×
+      </Link>
 
       <div className="login-card">
         <form className="login-form">
-          <h2 className='title'>Login to MakeWith</h2>
-          <input type="email" placeholder="Email" className='card'/>
-          <input type="password" placeholder="Password" />
-          <button type="submit" className="waitlist btn">Sign In</button>
+          <h2>Login to MakeWith</h2>
+          <input type="email" placeholder="Email" className="card" />
+          <input type="password" placeholder="Password" className="card" />
+          <button type="submit" className="btn">
+            <div className="sign-btn">Sign In</div>
+          </button>
         </form>
       </div>
     </div>
